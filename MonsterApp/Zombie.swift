795af42d3d6a70eea09c1a39339ad3c150ddabfd
ravisharmaa <bastola.ravi@gmail.com>
2018-10-28 16:31:47 +0545

    Access Modifiers

--- MonsterApp/Zombie.swift
+++ MonsterApp/Zombie.swift
@@ -12,13 +12,15 @@ class Zombie: Monster {
     
     var walkWithLimp = true
     
+    private (set) var isFallingApart = false
+    
     override class var spookyNoise: String {
         return "Brains...."
     }
     
     final override func terrorizeTown() {
         if let populaton = town?.population {
-            if populaton > 0 {
+            if populaton > 0 && isFallingApart {
                 town?.setTownPopulation(to: -10)
             }
         } else {
